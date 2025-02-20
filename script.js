8e17babf68e6a56ee66a4600dd1aa9885625d5bf
sinkvs <sinkevichvs@vk.com>
2025-02-20 14:16:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 const userAnswers = [];
 const questionsData = [
-    { question: "2 + 3 = ?", options: ['5', '4', '6'], correctAnswer: '5' },
-    { question: "4 * 5 = ?", options: ['20', '25', '18'], correctAnswer: '20' },
-    { question: "6 - 2 = ?", options: ['4', '3', '5'], correctAnswer: '4' },
-    { question: "3 * 7 = ?", options: ['21', '24', '19'], correctAnswer: '21' },
-    { question: "8 + 4 = ?", options: ['12', '10', '11'], correctAnswer: '12' },
+    { question: "2 + 3 = ?", options: ['4', '6', '5'], correctAnswer: '5' },
+    { question: "4 * 5 = ?", options: ['18', '20', '25'], correctAnswer: '20' },
+    { question: "6 - 2 = ?", options: ['3', '5', '4'], correctAnswer: '4' },
+    { question: "3 * 7 = ?", options: ['19', '24', '21'], correctAnswer: '21' },
+    { question: "8 + 4 = ?", options: ['11', '12', '10'], correctAnswer: '12' },
 ];
 
 let currentQuestionIndex = 0;
 
-function shuffleOptions(options) {
-    // Алгоритм Фишера-Йетса для перемешивания массива
-    for (let i = options.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [options[i], options[j]] = [options[j], options[i]];
-    }
-    return options;
-}
-
 function showQuestion(index) {
     const questionData = questionsData[index];
-    const shuffledOptions = shuffleOptions([...questionData.options]); // Перемешивание копии массива
-
     document.getElementById('questionText').innerText = questionData.question;
 
     const optionsContainer = document.getElementById('optionsContainer');
     optionsContainer.innerHTML = ''; // Очистка предыдущих вариантов
 
-    shuffledOptions.forEach((option) => {
+    questionData.options.forEach((option, idx) => {
         const button = document.createElement('button');
         button.innerText = option;
         button.onclick = () => selectAnswer(index, option);
